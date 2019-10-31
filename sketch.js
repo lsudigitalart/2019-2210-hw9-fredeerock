d1a97c8f8fa50e3ead942fca25f89db8a8ea33ad
Derick Ostrenko <dostrenko@gmail.com>
2019-10-31 13:22:37 -0500

    interaction

--- sketch.js
+++ sketch.js
@@ -3,12 +3,8 @@ var cars = [];
 function setup() {
   createCanvas(800, 600)
 
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 10; i++) {
     cars.push(new Car(random(5)));
-  }
-
-  for (var i = 0; i < 100; i++) {
-    cars[i].display();
   }
 
 }
@@ -16,8 +12,18 @@ function setup() {
 function draw() {
   background(100);
 
-  for (var i = 0; i < 100; i++) {
+  print(cars[0].yMov);
+
+  for (var i = 0; i < 10; i++) {
     cars[i].update();
+
+    if (mouseY > cars[i].yMov && mouseY < (cars[i].yMov + 20)) {
+      if (mouseX > cars[i].xPos && mouseX < (cars[i].xPos + 20)) {
+        cars[i].color = 0;
+        cars[i].speed = 0;
+      }
+    }
+
   }
 
 }
@@ -25,25 +31,15 @@ function draw() {
 function Car(tempSpeed) {
 
   this.speed = tempSpeed;
-  this.color = random(255);
+  this.color = random(100, 255);
   this.yMov = 0;
   this.xPos = random(width);
-
-  this.display = function () {
-
-  }
+  this.sqWidth = 20;
 
   this.update = function () {
-    fill(0, 0, this.color);
-    push();
-    translate(this.xPos, this.yMov, 0);
-    square(0, -20, 20);
     this.yMov += this.speed;
-    pop();
-  }
-
-  this.interaction = function () {
-
+    fill(0, 0, this.color);
+    square(this.xPos, this.yMov, this.sqWidth);
   }
 
 }
