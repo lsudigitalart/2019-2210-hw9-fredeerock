44db1fc41b0bd58de53e36cc1d3fe7ff3c21dc12
Derick Ostrenko <dostrenko@gmail.com>
2019-10-31 12:33:14 -0500

    cars

--- sketch.js
+++ sketch.js
@@ -1,58 +1,35 @@
-var myAnt1;
-var myAnt2;
-
-var randYPos1;
-var randYPos2;
+var myCar;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(800, 600)
+  background(100);
 
-  randYPos1 = random(height);
-  randYPos2 = random(height);
-
-  myAnt1 = new Ant(25, 100, randYPos1, 0, 2);
-  myAnt2 = new Ant(50, 250, randYPos2, 0, 5);
+  myCar = new Car(10, 200);
+  myCar.display();
 
 }
 
 function draw() {
-  background(100);
 
-  myAnt1.update();
-  myAnt1.display();
-
-  myAnt2.update();
-  myAnt2.display();
+  myCar.update();
 
 }
 
-function Ant(tempSize, tempColorR, tempY, tempX, tempSpeed) {
-  this.size = tempSize;
-  this.colorR = tempColorR;
-  this.yPos = tempY;
-  this.xPos = tempX;
+function Car(tempSpeed, tempColor) {
+
   this.speed = tempSpeed;
-
-  this.update = function () {
-    this.xPos = this.xPos + this.speed;
-
-    if (this.xPos > width) {
-      this.xPos = 0;
-    }
-
-  }
+  this.color = tempColor;
 
   this.display = function () {
-    fill(this.colorR, 0, 0);
-    circle(this.size * 0.5 + this.xPos, this.yPos, this.size);
-    circle(this.size * 1.5 + this.xPos, this.yPos, this.size);
-    circle(this.size * 2.5 + this.xPos, this.yPos, this.size);
+    fill(0, 0, this.color);
   }
 
+  this.update = function () {
+    square(10, 10, 20);
+  }
 
+  this.interaction = function () {
 
-  // this.ineract = function () {
-
-  // }
+  }
 
 }
